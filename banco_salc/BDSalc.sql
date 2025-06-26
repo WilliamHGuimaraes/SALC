8ac9d118fb1fc8eaf0c06b69dcc42a7c02cba69a
WilliamHGuimaraes <william.guimaraes@ufms.br>
2025-06-26 19:41:20 +0000

    commit 3

--- banco_salc/BDSalc.sql
+++ banco_salc/BDSalc.sql
@@ -60,3 +60,11 @@ CREATE TABLE Backup (
     id_backup INT PRIMARY KEY,
     data_backup TIMESTAMP
 );
+
+INSERT INTO Fornecedor VALUES (1, 'EMPRESA ALFA', '01.234.567/0001-01', 'empresa_alfa@gmail.com');
+INSERT INTO Fornecedor VALUES (2, 'EMPRESA BRAVO', '02.234.567/0001-02', 'empresa_bravo@gmail.com');
+INSERT INTO Fornecedor VALUES (3, 'EMPRESA CHARLIE', '03.234.567/0001-03', 'empresa_charlie@gmail.com');
+
+INSERT INTO Produto VALUES (1, 'Notebook', 'Notebook Dell 16GB RAM', 'unidade');
+INSERT INTO Produto VALUES (2, 'Rodo', 'Rodo cabo de madeira', 'unidade');
+INSERT INTO Produto VALUES (3, 'Panela', 'Panela de Ferro', 'unidade');
